07d1bb96ec353daa7c9b183b1c896fee688a852c
VanshajR <84905901+VanshajR@users.noreply.github.com>
2023-10-22 19:01:34 +0530

    Update addmat.c
    Signed-off-by: VanshajR <84905901+VanshajR@users.noreply.github.com>

--- addmat.c
+++ addmat.c
@@ -2,6 +2,7 @@
 int main() {
   int r, c, a[100][100], b[100][100], sum[100][100];
   int i, j;
+  //The matrices must have equal number of rows and columns
   printf("Enter the number of rows (between 1 and 100): ");
   scanf("%d", &r);
   printf("Enter the number of columns (between 1 and 100): ");
